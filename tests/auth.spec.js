ccf1a41f9a99a2640fd27f10d174868e04737380
kmutsolgov <kmutsolgov@gmail.com>
2023-04-19 23:18:39 -0400

    added beforeHook

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -4,18 +4,19 @@ import 'dotenv/config'
 
 describe('Autorization test', ()=>{
     describe('Autorization with valid data', ()=>{
+        let response
+
+        before(async()=>{
+            response = await request(process.env.BASE_URL)
+                .post('/v5/user/login')
+                .send({email: process.env.EMAIL, password: process.env.PASSWORD})
+        })
 
         it('Response status code is 200', async()=>{
-            let res = await request(process.env.BASE_URL)
-                .post('/v5/user/login')
-                .send({email: process.env.EMAIL, password: process.env.PASSWORD})
-            expect(res.statusCode).to.eq(200)
+            expect(response.statusCode).to.eq(200)
         })
         it('Response body returns correct message', async()=>{
-            let res = await request(process.env.BASE_URL)
-                .post('/v5/user/login')
-                .send({email: process.env.EMAIL, password: process.env.PASSWORD})
-            expect(res.body.message).to.eq('Auth success')
+            expect(response.body.message).to.eq('Auth success')
         })
     })
 })
