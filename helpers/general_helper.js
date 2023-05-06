8bad67dfdd1c90ba815337f4dfa4e1fb0765d843
kmutsolgov <kmutsolgov@gmail.com>
2023-05-05 22:48:06 -0400

    registration tests update

--- helpers/general_helper.js
+++ helpers/general_helper.js
@@ -1,5 +1,5 @@
 import request from "supertest";
-import {faker} from "@faker-js/faker";
+import 'dotenv/config'
 
 function logIn (email, password){
     return request(process.env.BASE_URL)
@@ -11,7 +11,7 @@ function register (companyName, firstName, lastName, email, password){
     return request(process.env.BASE_URL)
         .post('/v5/user')
         .send({
-            companyName: companyName ,
+            companyName: companyName,
             firstName: firstName,
             lastName: lastName,
             email: email,
