adb99fd2ca817b6c73d65c38483ff2b8ddf1a6cd
kmutsolgov <kmutsolgov@gmail.com>
2023-05-19 20:41:17 -0400

    various tests

--- tests/registration.spec.js
+++ tests/registration.spec.js
@@ -1,8 +1,7 @@
-import { expect } from 'chai'
+import {expect} from 'chai'
 import 'dotenv/config'
 import { faker } from '@faker-js/faker'
-import {logIn, register} from '../helpers/general_helper'
-import request from "supertest";
+import {register} from '../helpers/general_helper'
 
 describe('Registration test', () => {
   describe('Register/create news user with valid credentials', () => {
@@ -151,60 +150,7 @@ describe('Registration test', () => {
       expect(response.body.message).to.eq('User with this e-mail exists')
     })
   })
-
-  describe("Confirm email verification", async()=>{
-    let response
-
-    before(async()=>{
-      response = await request(process.env.BASE_URL)
-          .post('/email/search')
-          .send({email:'ganizade06@gmail.com'})
-    })
-
-    it('Response status code is 200', ()=>{
-      expect(response.statusCode).to.eq(200)
-    })
-    it('Response body message returns a correct message', ()=>{
-      expect(response.body.message).to.eq('EmailSearch ok')
-    })
-  })
-
-  describe('Get user by ID', async()=>{
-    let response
-    let id
-    let getUser
-    before(async()=>{
-      response = await logIn(process.env.EMAIL, process.env.PASSWORD)
-      id = response.body.payload.user._id
-      console.log(id)
-      getUser = await request(process.env.BASE_URL)
-          .get(`/v5/user/${id}`)
-          .set('Authorization', process.env.TOKEN)
-      console.log(getUser)
-    })
-    it('Response code is 200', ()=>{
-      expect(getUser.statusCode).to.eq(200)
-    })
-    it('Response body message returns a correct message', ()=>{
-      expect(getUser.body.message).to.eq('User found')
-    })
-  })
-  describe('Get all users', async()=>{
-    let response
-    before(async()=>{
-      response = await request (process.env.BASE_URL)
-          .post('/v5/user/search')
-          .send({limit: 100, page: 1})
-          .set('Authorization', process.env.TOKEN)
-    })
-    it('Response code is 200', ()=>{
-      expect(response.statusCode).to.eq(200)
-    })
-    it('Response body contains users array', ()=>{
-      expect(response.body).to.be.an('object')
-    })
-    it('Response body does not show password', async()=>{
-      expect(response.body.items).to.be.an('array')
-    })
-  })
 })
+
+
+
